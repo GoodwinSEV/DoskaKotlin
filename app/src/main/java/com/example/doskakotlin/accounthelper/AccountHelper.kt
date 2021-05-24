88248dc467b190ffa98be0b4ae20b5d671f0378c
Gogs <gogs@fake.local>
2021-05-24 09:49:49 +0900

    Урок 9

--- app/src/main/java/com/example/doskakotlin/accounthelper/AccountHelper.kt
+++ app/src/main/java/com/example/doskakotlin/accounthelper/AccountHelper.kt
@@ -1,14 +1,15 @@
 package com.example.doskakotlin.accounthelper
 
+import android.util.Log
 import android.widget.Toast
 import com.example.doskakotlin.MainActivity
 import com.example.doskakotlin.R
+import com.example.doskakotlin.constants.FirebaseAuthConstants
 import com.example.doskakotlin.dialoghelper.GoogleAccConst
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 
 class AccountHelper (act:MainActivity){
     private val act = act
@@ -24,10 +25,31 @@ class AccountHelper (act:MainActivity){
                     sendEmailVerification(task.result?.user!!)
                     act.uiUpdate(task.result?.user!!)
                 }
-                else
-                {
-                    Toast.makeText(act, act.resources.getString(R.string.sign_up_error), Toast.LENGTH_LONG).show()
+                else {
+                    //Toast.makeText(act, act.resources.getString(R.string.sign_up_error), Toast.LENGTH_LONG).show()
+                    //Log.d("LogISIP", "Exception: +" + task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException){
+                        val exception = task.exception as FirebaseAuthInvalidCredentialsException
+                        //Log.d("LogISIP", "Exception: ${exception.errorCode}" )
+                        if (exception.errorCode == FirebaseAuthConstants.ERROR_INVALID_EMAIL) {
+                            Toast.makeText(act, FirebaseAuthConstants.ERROR_INVALID_EMAIL, Toast.LENGTH_LONG).show()
+                        }
+                } else
+                    if (task.exception is FirebaseAuthWeakPasswordException) {
+                        val exception = task.exception as FirebaseAuthWeakPasswordException
+                        //Log.d("LogISIP", "Exception: ${exception.errorCode}" )
+                        if (exception.errorCode == FirebaseAuthConstants.ERROR_WEAK_PASSWORD) {
+                                Toast.makeText(act, FirebaseAuthConstants.ERROR_WEAK_PASSWORD, Toast.LENGTH_LONG).show()
+                            }
+                } else
+                    if (task.exception is FirebaseAuthWeakPasswordException) {
+                        val exception = task.exception as FirebaseAuthUserCollisionException
+                        Log.d("LogISIP", "Exception: ${exception.errorCode}" )
+                        //if (exception.errorCode == FirebaseAuthConstants.ERROR_WEAK_PASSWORD) {
+                                 //   Toast.makeText(act, FirebaseAuthConstants.ERROR_WEAK_PASSWORD, Toast.LENGTH_LONG).show()
+                         //       }
                 }
+                    }
             }
         }
     }
@@ -41,7 +63,13 @@ class AccountHelper (act:MainActivity){
                 }
                 else
                 {
-                    Toast.makeText(act, act.resources.getString(R.string.sign_in_error), Toast.LENGTH_LONG).show()
+                    if (task.exception is FirebaseAuthInvalidCredentialsException){
+                        val exception = task.exception as FirebaseAuthInvalidCredentialsException
+                        Log.d("LogISIP", "Exception: ${exception.errorCode}" )
+                        if (exception.errorCode == FirebaseAuthConstants.ERROR_WRONG_PASSWORD) {
+                            Toast.makeText(act, FirebaseAuthConstants.ERROR_WRONG_PASSWORD, Toast.LENGTH_LONG).show()
+                        }
+                    }
                 }
             }
         }
@@ -61,7 +89,7 @@ class AccountHelper (act:MainActivity){
 
     private fun getSignInClient():GoogleSignInClient{
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
-        requestIdToken(act.getString(R.string.default_web_client_id)).build()
+        requestIdToken(act.getString(R.string.default_web_client_id)).requestEmail().build()
         return GoogleSignIn.getClient(act, gso)
     }
 
@@ -76,7 +104,11 @@ class AccountHelper (act:MainActivity){
         act.mAuth.signInWithCredential(credential).addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 Toast.makeText(act, "Sign in done ", Toast.LENGTH_LONG).show()
+                act.uiUpdate(task.result?.user)
+            } else {
+                Log.d("LogISIP", "Google Sign In Exception : ${task.exception}" )
             }
+
         }
     }
 }
---
+++ app/src/main/java/com/example/doskakotlin/constants/FirebaseAuthConstants.kt
@@ -0,0 +1,8 @@
+package com.example.doskakotlin.constants
+
+object FirebaseAuthConstants {
+    const val ERROR_INVALID_EMAIL = "ERROR_INVALID_EMAIL"
+    const val ERROR_WEAK_PASSWORD = "ERROR_WEAK_PASSWORD"
+    const val ERROR_WRONG_PASSWORD = "ERROR_WRONG_PASSWORD"
+  //  const val
+}
